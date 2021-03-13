08584dfcf551e931a1b23ad967b41d869d1617e9
PointCloudYC <cyinac@connect.ust.hk>
2021-03-13 11:19:40 +0800

    add setup installation script for conda

---
+++ requirements-install.sh
@@ -0,0 +1,8 @@
+conda install -c anaconda msgpack-numpy -y
+conda install -c conda-forge python-lmdb -y
+conda install -c anaconda h5py -y
+conda install -c conda-forge hydra-core=0.11 -y
+conda install -c conda-forge pytorch-lightning=0.7 -y
+
+# install custom ops (require cuda 10.1)
+# pip3 install pointnet2_ops_lib/.
